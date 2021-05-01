5faf8b051350dddabf08afd4bc086cf13a061387
Joshua Gdovin <jgdovin@accusoft.com>
2021-05-01 13:59:53 -0400

    add active cell layer

--- src/Canvas.tsx
+++ src/Canvas.tsx
@@ -3,9 +3,11 @@ import fixDpi from './helpers/fixDpi';
 export default function Canvas() {
     const backgroundCanvasRef = useRef<HTMLCanvasElement>(document.createElement('canvas'));
     const drawingCanvasRef = useRef<HTMLCanvasElement>(document.createElement('canvas'));
+    const activeCellCanvasRef = useRef<HTMLCanvasElement>(document.createElement('canvas'));
     const pixelSize = 25;
     const canvasWidth = window.innerWidth;
     const canvasHeight = window.innerHeight;
+    
     const width = 32;
     const height = 32;
     const drawingOffsetLeft = Math.round(canvasWidth / 2 - ((width * pixelSize) / 2));
@@ -15,6 +17,19 @@ export default function Canvas() {
 
     const [isDragging, setIsDragging] = useState(false);
     const latestDragState = useRef(isDragging);
+
+    const [activeCell, setActiveCell] = useState('');
+
+    useEffect(() => {
+        const activeCellCtx = activeCellCanvasRef.current.getContext('2d');
+        activeCellCtx?.clearRect(0, 0, drawingHeight, drawingWidth);
+        if (activeCell.length <= 0) { return; }
+        fixDpi(activeCellCanvasRef.current);
+        const [x, y] = activeCell.split(',');
+        activeCellCtx!.fillStyle = '#000000';
+        activeCellCtx!.fillRect(parseInt(x) * pixelSize, parseInt(y) * pixelSize, pixelSize, pixelSize);
+    }, [activeCell, drawingHeight, drawingWidth]);
+
     useEffect(() => {
         latestDragState.current = isDragging;
     }, [isDragging])
@@ -27,8 +42,8 @@ export default function Canvas() {
         backgroundCtx!.fillStyle = '#000000';
         backgroundCtx!.fillRect(0, 0, canvasWidth, canvasHeight);
         drawCtx!.fillStyle = '#ffffff';
-        drawCtx!.fillRect(0, 0, width * pixelSize, height * pixelSize);
-    }, [canvasHeight, canvasWidth]);
+        drawCtx!.fillRect(0, 0, drawingWidth, drawingHeight);
+    }, [canvasHeight, canvasWidth, drawingHeight, drawingWidth]);
 
     useEffect(() => {
         const drawCtx = drawingCanvasRef.current.getContext('2d');
@@ -44,11 +59,18 @@ export default function Canvas() {
         }
         const handleMouseMove = {
             handleEvent(e: any) {
-                if (!latestDragState.current && e.type !== 'click') { return; }
                 const x = Math.floor((e.pageX - drawingOffsetLeft) / pixelSize);
                 const y = Math.floor((e.pageY - drawingOffsetTop) / pixelSize);
+                setActiveCell(`${x},${y}`);
+                if (!latestDragState.current && e.type !== 'click') { return; }
                 drawCtx!.fillStyle = '#000000';
                 drawCtx!.fillRect(x * pixelSize, y * pixelSize, pixelSize, pixelSize);
+            }
+        }
+        const handleMouseLeave = {
+            handleEvent(e: any) {
+                console.log('left');
+                setActiveCell('');
             }
         }
 
@@ -56,12 +78,14 @@ export default function Canvas() {
         drawingCanvasRef.current.addEventListener('mouseup', handleMouseUp);
         drawingCanvasRef.current.addEventListener('mousemove', handleMouseMove);
         drawingCanvasRef.current.addEventListener('click', handleMouseMove);
+        drawingCanvasRef.current.addEventListener('mouseleave', handleMouseLeave);
     }, [drawingOffsetLeft, drawingOffsetTop, isDragging]);
 
     return (
         <div>
             <canvas ref={backgroundCanvasRef} className="background" style={{position: 'absolute', left: '0px', top: '0px', width: canvasWidth, height: canvasHeight}} />
             <canvas ref={drawingCanvasRef} className="draw" style={{position: 'absolute', left: drawingOffsetLeft, top: drawingOffsetTop, width: drawingWidth, height: drawingHeight}} />
+            <canvas ref={activeCellCanvasRef} className="activeCell" style={{pointerEvents: 'none', position: 'absolute', left: drawingOffsetLeft, top: drawingOffsetTop, width: drawingWidth, height: drawingHeight}} />
         </div>
     );
 };
