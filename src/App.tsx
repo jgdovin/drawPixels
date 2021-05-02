e8f628e6b808091ac3c7807aef86a5ff934cd5b4
Joshua Gdovin <jgdovin@accusoft.com>
2021-05-02 12:10:57 -0400

    fix error

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import Canvas from './Canvas';
 
 import { makeStyles } from '@material-ui/core/styles';
